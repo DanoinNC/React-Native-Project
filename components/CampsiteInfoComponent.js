bc2ffdc6a2fc82ef9b92dee9a117bca8461cc78d
Dan Parrott <spento@aol.com>
2022-03-13 19:47:50 -0400

    Week 3 Workshop

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -28,6 +28,7 @@ function RenderCampsite(props) {
     const view = React.createRef();
 
     const recognizeDrag = ({dx}) => (dx < -200) ? true : false;
+    const recognizeComment = ({dx}) => (dx > 200) ? true : false;
 
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
@@ -55,7 +56,10 @@ function RenderCampsite(props) {
                     ],
                     { cancelable: false }
                 );
-            }
+            }   else if(recognizeComment(gestureState)) {
+                    console.log('pan responder end', gestureState);
+                    props.onShowModal();
+                } 
             return true;
         }
     });
